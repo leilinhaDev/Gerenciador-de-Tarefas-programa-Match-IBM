2276959dd2a6fe997b28cdf6feeb83b52e755c3b
leilinhaDev <leilinhasds@hotmail.com>
2024-03-17 18:13:13 -0300

    Criação do modelo de Tarefas

--- gerenciadorDeTarefasDjango/tarefas/models.py
+++ gerenciadorDeTarefasDjango/tarefas/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Tarefa(models.Model):
+    descricao = models.CharField(max_length = 255, null = False)
+    dataVencimento = models.DateField(null = False)
+    prioridadeEscolhas = (
+        (1, 'Baixa'),
+        (2, 'Media'),
+        (3, 'Alta')
+    )
+    prioridade  = models.IntegerField(null = False, default = 2, choices = prioridadeEscolhas)
+    finalizada = models.BooleanField(default = False)
+
+    def __str__(self) -> str:
+        return f"{self.descricao}"
