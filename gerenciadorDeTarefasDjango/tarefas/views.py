58e2ef5636e0ca947cc090cb98273c14cf680f24
leilinhaDev <leilinhasds@hotmail.com>
2024-03-19 15:55:30 -0300

    Criando e modificando o css do projeto. Divisão dos cards das tarefas a fazer e das tarefas feitas

--- gerenciadorDeTarefasDjango/tarefas/views.py
+++ gerenciadorDeTarefasDjango/tarefas/views.py
@@ -2,25 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Tarefa
-from datetime import datetime
+
 
 
 def home(request):
-   # tarefa1 = Tarefa(descricao = 'lavar prato', dataVencimento = datetime.now().date())
-   # tarefa2 = Tarefa(descricao = 'tarefa feita', dataVencimento = datetime.now().date(), finalizada = True)
-   # tarefa1.save()
-   # tarefa2.save()
-   
     feitas = Tarefa.objects.filter(finalizada = True)
     naoFeitas = Tarefa.objects.filter(finalizada = False)
- 
     template = loader.get_template('home.html')
     context = {
           'feitas': feitas,
           'naoFeitas': naoFeitas,
     }
-    
-
     return HttpResponse(template.render(context, request))
 
    
