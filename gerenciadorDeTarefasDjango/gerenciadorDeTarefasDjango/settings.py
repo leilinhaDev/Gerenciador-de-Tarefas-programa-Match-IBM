9637dbc31c59d05a9040c223d1b0502bbbd6ccd5
leilinhaDev <leilinhasds@hotmail.com>
2024-03-24 20:57:09 -0300

    melhoria no static files para integração com produção

--- gerenciadorDeTarefasDjango/gerenciadorDeTarefasDjango/settings.py
+++ gerenciadorDeTarefasDjango/gerenciadorDeTarefasDjango/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3*jq(-vy^89@b+t4w6!n0=iqoawe(2-&(+(d*1&l@#+e#ljn$n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['leilinhasds.pythonanywhere.com']
+ALLOWED_HOSTS = ['leilinhasds.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
@@ -115,6 +115,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = 'static/'
 
