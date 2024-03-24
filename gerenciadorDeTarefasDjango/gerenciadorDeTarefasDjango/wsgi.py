ed5000c41b9f92f27ded8bf4c17fa4cd24c04085
leilinhaDev <leilinhasds@hotmail.com>
2024-03-24 20:12:39 -0300

    wsgi original

--- gerenciadorDeTarefasDjango/gerenciadorDeTarefasDjango/wsgi.py
+++ gerenciadorDeTarefasDjango/gerenciadorDeTarefasDjango/wsgi.py
@@ -7,26 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-'''import os
+import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gerenciadorDeTarefasDjango.settings')
 
-application = get_wsgi_application()'''
-
-import os
-import sys
-
-# Adicione o diretório do seu projeto Django ao sys.path
-path = '/home/leilinhasds/Gerenciador-de-Tarefas-programa-Match-IBM/gerenciadorDeTarefasDjango/gerenciadorDeTarefasDjango'
-if path not in sys.path:
-    sys.path.append(path)
-
-# Configure as configurações do Django
-os.environ['DJANGO_SETTINGS_MODULE'] = 'gerenciadorDeTarefasDjango.settings'
-
-# Obtenha a aplicação WSGI do Django
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
